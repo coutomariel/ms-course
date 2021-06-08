6d0d362d14ad2db48658e1c533eceb6fdc37b3ac
Mariel Couto <coutomariel@gmail.com>
2021-06-08 14:25:06 -0300

    Implementação de comunicação fazendo chamada a outro serciço usando RestTemplate

--- hr-payroll/src/main/java/com/coutomariel/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/coutomariel/hrpayroll/services/PaymentService.java
@@ -1,13 +1,30 @@
 package com.coutomariel.hrpayroll.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.coutomariel.hrpayroll.entities.Payment;
+import com.coutomariel.hrpayroll.entities.Worker;
 
 @Service
 public class PaymentService {
 
+	@Value("${hr-worker.host}")
+	private String workerHost;
+
+	@Autowired
+	private RestTemplate restTemplate;
+
 	public Payment getPayment(long workerId, int days) {
-		return new Payment("Bob", 200.00, days);
+		Map<String, String> params = new HashMap<String, String>();
+		params.put("id", "" + workerId);
+
+		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, params);
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
