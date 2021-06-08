5091225d7a34835933da03a71a47b9e8bfb214bd
Mariel Couto <coutomariel@gmail.com>
2021-06-08 14:53:10 -0300

    Implementação de comunicação com serviço externo usando OpenFeign

--- hr-payroll/src/main/java/com/coutomariel/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/coutomariel/hrpayroll/services/PaymentService.java
@@ -1,30 +1,20 @@
 package com.coutomariel.hrpayroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.coutomariel.hrpayroll.entities.Payment;
 import com.coutomariel.hrpayroll.entities.Worker;
+import com.coutomariel.hrpayroll.feignclients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
 
-	@Value("${hr-worker.host}")
-	private String workerHost;
-
 	@Autowired
-	private RestTemplate restTemplate;
+	private WorkerFeignClient workerFeignClient;
 
 	public Payment getPayment(long workerId, int days) {
-		Map<String, String> params = new HashMap<String, String>();
-		params.put("id", "" + workerId);
-
-		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, params);
+		Worker worker = workerFeignClient.findById(workerId).getBody();
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
